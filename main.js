3eea2e4f3c06e730b85f6234954bea3df4abea28
GustavoAbranches <gusabranches@gmail.com>
2023-11-16 22:34:05 -0300

    added functions game and fix the playRound function error

--- main.js
+++ main.js
@@ -10,28 +10,35 @@ function getComputerChoice(){
 
 function playRound(playerSelection, computerSelection){
 
-    if(playerSelection === 'paper' && computerSelection === 'rock'){
+    const player = playerSelection.toLowerCase()
+    const computer = computerSelection.toLowerCase()
+
+    if(player === 'paper' && computer === 'rock'){
         return 'You win! Paper beats Rock'
     }
-    else if(playerSelection === 'rock' && computerSelection === 'scissor'){
+    else if(player === 'rock' && computer === 'scissor'){
       return 'You win! Rock beats Paper'
     }
-    else if(playerSelection === 'scissor' && computerSelection === 'paper'){
+    else if(player === 'scissor' && computer === 'paper'){
         return 'You win! Scissor beats Paper'
     }
-    else if(playerSelection === 'paper' && computerSelection === 'scissor'){
+    else if(player === 'paper' && computer=== 'scissor'){
         return 'You lose! Scissor beats Paper'
     }
-    else if(playerSelection === 'scissor' && computerSelection === 'rock'){
+    else if(player === 'scissor' && computer === 'rock'){
         return 'You lose! Scissor beats Paper'
     }
-    else if(playerSelection === 'rock' && computerSelection === 'paper'){
+    else if(player === 'rock' && computer === 'paper'){
         return 'You lose! Paper beats Rock'
     }
     else{
         return 'is a tie'
     }
    
+}
+
+function game(){
+    
 }
 
 const playerChoice = prompt('Choose between: Rock, Paper, Scissor')
