48df111964535cd1eac6ebeb9c3a9bc78d721445
GustavoAbranches <gusabranches@gmail.com>
2023-11-17 18:07:52 -0300

    the game function has been completed

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ function playRound(playerSelection, computerSelection){
     else if(player === 'scissor' && computer === 'paper'){
         return 'You win! Scissor beats Paper'
     }
-    else if(player === 'paper' && computer=== 'scissor'){
+    else if(player === 'paper' && computer === 'scissor'){
         return 'You lose! Scissor beats Paper'
     }
     else if(player === 'scissor' && computer === 'rock'){
@@ -32,17 +32,49 @@ function playRound(playerSelection, computerSelection){
         return 'You lose! Paper beats Rock'
     }
     else{
-        return 'is a tie'
+        return 'It\'s a tie!'
     }
    
 }
 
 function game(){
-    
+
+    let roundCount = 0
+    let playerPoints = 0
+    let computerPoints = 0
+
+    while (roundCount < 5){
+
+        const playerChoice = prompt('Choose between: Rock, Paper, Scissor')
+        const computerChoice = getComputerChoice()
+        const roundResult = playRound (playerChoice, computerChoice)
+
+        console.log('Player choice: ' + playerChoice)
+        console.log('Computer choice ' + computerChoice)
+        console.log('Round result: ' + roundResult)
+        
+        if (roundResult.includes('win')){
+            playerPoints++
+        } else if(roundResult.includes('lose')){
+            computerPoints++
+        }
+
+        console.log('Player Points: ' + playerPoints)
+        console.log('Computer points: ' 
+        + computerPoints)
+
+        roundCount++
+        console.log('Round count: ' + roundCount)
+    }
+
+    if (playerPoints > computerPoints){
+        console.log('You win!!!')
+    } else if(playerPoints < computerPoints){
+        console.log('You lose!!!')
+    } else{
+        console.log('It\'s a tie in the game!')
+    }
 }
 
-const playerChoice = prompt('Choose between: Rock, Paper, Scissor')
-const computerChoice = getComputerChoice()
-const roundChoice = playRound (playerChoice, computerChoice)
-console.log(computerChoice)
-console.log(roundChoice)
+const gameResult = game()
+console.log(gameResult)
